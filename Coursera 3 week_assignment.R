88f5e25d887ba393e63866f588f9592e3aed422e
artursunov <72069939+artursunov@users.noreply.github.com>
2020-10-16 17:55:05 +0300

    Add files via upload

---
+++ Coursera 3 week_assignment.R
@@ -0,0 +1,34 @@
+makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) m <<- inverse
+  getinverse <- function() m
+  list(set = set,
+       get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+}
+
+cacheSolve <- function(x, ...) {
+  m <- x$getinverse()
+  if (!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
+}
+
+
+B <- matrix(c(5,10,7, 10),2,2)
+B1 <- makeCacheMatrix(B)
+
+cacheSolve(B1)
+
+cacheSolve(B1)
